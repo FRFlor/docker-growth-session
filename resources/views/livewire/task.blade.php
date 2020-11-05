10da37985c6412b585632e527f5167d97a966bd6
Felipe Flor <f.flor@vehikl.com>
2020-11-05 00:51:49 -0500

    Tweak styling

--- resources/views/livewire/task.blade.php
+++ resources/views/livewire/task.blade.php
@@ -1,5 +1,5 @@
 <li class="my-4 p-4 flex justify-between border border-gray-200 text-gray-800 shadow">
-    <p>{{$task->description}}</p>
+    <p class="select-none">{{$task->description}}</p>
 
     <div class="flex items-center">
         <label for="task_{{$task->id}}" class="sr-only">
@@ -8,7 +8,7 @@
         <input id="task_{{$task->id}}" type="checkbox"
                wire:click="toggleIsComplete"
                class="h-8 w-8 cursor-pointer" {{$task->is_complete ? 'checked="checked"' : ''}}">
-        <button class="border border-red-500 h-8 w-8 rounded-sm ml-4" wire:click="delete">
+        <button class="border border-red-500 h-8 w-8 rounded-sm ml-4 hover:bg-red-100" wire:click="delete">
             <i class="fas fa-trash text-red-500"></i>
         </button>
     </div>
